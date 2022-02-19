73213af53ab005b84d8c77cc81e30ff1b118ca05
Oscar Bocanegra <85693186+obocanegra-dev@users.noreply.github.com>
2022-02-19 09:35:18 -0300

    Delete 026-map-marker directory

--- 026-map-marker/script.js
+++
@@ -1,9 +0,0 @@
-$('.marker').bind('click', function() {
-	$('.card').addClass('active');
-	$('.marker').addClass('inactive');
-});
-
-$('.card').bind('click', function() {
-	$('.card').removeClass('active');
-	$('.marker').removeClass('inactive');
-});
